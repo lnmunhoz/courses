6583c5e8a856362053827414f15db4b4568a2bc0
lnmunhoz <ln.munhoz@gmail.com>
2016-06-07 01:37:31 -0300

    Child Properties. Lesson 8

--- Egghead/React Fundamentals/App.js
+++ Egghead/React Fundamentals/App.js
@@ -1,57 +1,17 @@
 import React  from 'react';
 import ReactDOM from 'react-dom';
 class App extends React.Component {
-	constructor() {
-		super();
-		this.state = {
-			txt: 'this is the state txt',
-			red: 0,
-			blue: 0,
-			green: 0
-		}
-	}
-	update(e) {
-		this.setState({
-			red: ReactDOM.findDOMNode(this.refs.red).value,
-			blue: ReactDOM.findDOMNode(this.refs.blue).value,
-			green: ReactDOM.findDOMNode(this.refs.green).value,
-		})
-	}
-	render(){
-		return (
-			<div>
-				<Slider ref="red" update={this.update.bind(this)} />
-				{this.state.red}
-				<br />
-				<Slider ref="blue" update={this.update.bind(this)} />
-				{this.state.blue}
-				<br />
-				<Slider ref="green" update={this.update.bind(this)} />
-				{this.state.green}
-				<br />
-			</div>
-		) 
-	}
-}
-
-class Slider extends React.Component {
 	render() {
-		return (
-			<input type="range"
-				min="0"
-				max="255"
-				onChange={this.props.update} />
-		)
+		return <Button> I <Heart /> React </Button>
 	}
 }
 
-App.propTypes = {
-	txt: React.PropTypes.string.isRequired,
-	number: React.PropTypes.number
+class Button extends React.Component {
+	render(){
+		return <button>{this.props.children}</button>
+	}
 }
 
-App.defaultProps = {
-	txt: 'this is the default text',
-}
+const Heart = () => <span> Love </span>
 
 export default App;
